5b0138c41f68492d1c6ee36c838c2551f31c948f
Lert <tplermit@gmail.com>
2024-02-23 14:38:25 -0400

    Add imports and metadata for invoices and layout pages

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,11 +1,18 @@
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { fetchInvoicesPages } from '@/app/lib/data';    
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
+
 import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { fetchInvoicesPages } from '@/app/lib/data';    
-import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+}
 
 export default async function Page({
   searchParams,
